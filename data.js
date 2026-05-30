f3c094e473714b4855b2ba830b175a035d5ee103
tpskDanya <88769199+tpskdanya@users.noreply.github.com>
2026-05-30 15:05:02 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,42 +1,42 @@
 const salaryData = {
   "Ставрополь": {
-    "Junior Sales Expert": 185,
-    "Sales Expert": 217,
-    "Senior Sales Expert": 261
+    "Junior Sales Expert": 199,
+    "Sales Expert": 233,
+    "Senior Sales Expert": 279
   },
   "Пятигорск": {
-    "Junior Sales Expert": 185,
-    "Sales Expert": 217,
-    "Senior Sales Expert": 261
+    "Junior Sales Expert": 199,
+    "Sales Expert": 233,
+    "Senior Sales Expert": 279
   },
-  "Есскнтуки": {
-    "Junior Sales Expert": 185,
-    "Sales Expert": 217,
-    "Senior Sales Expert": 261
+  "Ессентуки": {
+    "Junior Sales Expert": 199,
+    "Sales Expert": 233,
+    "Senior Sales Expert": 279
   },
   "Георгиевск": {
-    "Junior Sales Expert": 185,
-    "Sales Expert": 217,
-    "Senior Sales Expert": 261
+    "Junior Sales Expert": 199,
+    "Sales Expert": 233,
+    "Senior Sales Expert": 279
   },
   "Кисловодск": {
-    "Junior Sales Expert": 185,
-    "Sales Expert": 217,
-    "Senior Sales Expert": 261
+    "Junior Sales Expert": 199,
+    "Sales Expert": 233,
+    "Senior Sales Expert": 279
   },
   "Нальчик": {
-    "Junior Sales Expert": 185,
-    "Sales Expert": 217,
-    "Senior Sales Expert": 261
+    "Junior Sales Expert": 199,
+    "Sales Expert": 233,
+    "Senior Sales Expert": 279
   },
   "Владикавказ": {
-    "Junior Sales Expert": 185,
-    "Sales Expert": 217,
-    "Senior Sales Expert": 261
+    "Junior Sales Expert": 199,
+    "Sales Expert": 233,
+    "Senior Sales Expert": 279
   },
   "Махачкала": {
-    "Junior Sales Expert": 185,
-    "Sales Expert": 217,
-    "Senior Sales Expert": 261
+    "Junior Sales Expert": 199,
+    "Sales Expert": 233,
+    "Senior Sales Expert": 279
   }
 };
