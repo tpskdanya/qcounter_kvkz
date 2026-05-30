c988c32d1374048c24bf6bc5e263d4db41327b19
tpskDanya <88769199+tpskdanya@users.noreply.github.com>
2026-05-30 14:52:42 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,15 +1,40 @@
 const salaryData = {
-  "Волгоград": {
+  "Ставрополь": {
     "Junior Sales Expert": 185,
     "Sales Expert": 217,
     "Senior Sales Expert": 261
   },
-  "Волжский": {
+  "Пятигорск": {
     "Junior Sales Expert": 185,
     "Sales Expert": 217,
     "Senior Sales Expert": 261
   },
-  "Астрахань": {
+  "Есскнтуки": {
+    "Junior Sales Expert": 185,
+    "Sales Expert": 217,
+    "Senior Sales Expert": 261
+  }
+  "Георгиевск": {
+    "Junior Sales Expert": 185,
+    "Sales Expert": 217,
+    "Senior Sales Expert": 261
+  }
+  "Кисловодск": {
+    "Junior Sales Expert": 185,
+    "Sales Expert": 217,
+    "Senior Sales Expert": 261
+  }
+  "Нальчик": {
+    "Junior Sales Expert": 185,
+    "Sales Expert": 217,
+    "Senior Sales Expert": 261
+  }
+  "Владикавказ": {
+    "Junior Sales Expert": 185,
+    "Sales Expert": 217,
+    "Senior Sales Expert": 261
+  }
+  "Махачкала": {
     "Junior Sales Expert": 185,
     "Sales Expert": 217,
     "Senior Sales Expert": 261
